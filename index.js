38915f0090c4c231057f99eaeb103cac4dcd5f23
Lucas Eckman <lucas.eckman@gmail.com>
2019-02-07 10:21:34 -0500

    contact inputs laid out

--- index.js
+++ index.js
@@ -37,14 +37,14 @@ function validateStepTwoInfoSubmit(e) {
 
 var stepOneCard = $("#basic-information-card");
 var stepTwoCard = $("#contact-information-card");
-var stepThreeCard = $("#extra-card");
+var stepThreeCard = $("#additional-card");
 
 $("#step-two-back-button").click(stepTwoBackToThree); // No validation for navigating backwards
 $("#step-three-back-button").click(stepThreeBackToOne); // No validation for navigating backwards
 
 var stepperHeaderStepOne = $("#stepper-basic-header");
 var stepperHeaderStepTwo = $("#stepper-contact-header");
-var stepperHeaderStepThree = $("#stepper-extra-header");
+var stepperHeaderStepThree = $("#stepper-additional-header");
 
 function stepOneToTwo() {
   stepOneCard.hide();
@@ -79,57 +79,72 @@ function stepThreeBackToOne() {
   $("#first-name").focus();
 }
 
+// Load and Fill Form Data
+
+function loadFormData() {
+  var data = {
+    billingData: {},
+    paymentData: {}
+  };
+  billingForm.serializeArray().forEach(function(ele) {
+    data.billingData[ele.name] = ele.value;
+  });
+  paymentForm.serializeArray().forEach(function(ele) {
+    data.paymentData[ele.name] = ele.value;
+  });
+  return data;
+}
+
+function appendFormData(data) {
+  var billingDetails =
+    '<p class="mb-0">' +
+    data.billingData.firstName +
+    " " +
+    data.billingData.lastName +
+    '</p><p class="mb-0">' +
+    data.billingData.streetAddress +
+    '</p><p class="mb-0' +
+    (data.billingData.streetAddress2
+      ? '">' + data.billingData.streetAddress2
+      : ' d-none d-sm-block d-md-none d-lg-block">' + "<span>&nbsp</span>") +
+    '</p><p class="mb-0">' +
+    data.billingData.city +
+    ", " +
+    data.billingData.state +
+    '</p><p class="mb-0">' +
+    data.billingData.zip +
+    "</p>";
+  var cardNumber = data.paymentData.cardNumber;
+  var paymentDetails =
+    '<p class="mb-0">' +
+    data.paymentData.cardName +
+    '</p><p class="mb-0">' +
+    (cardNumber.length === 15
+      ? "**** ****** *" + cardNumber.slice(14)
+      : "**** **** **** " + cardNumber.slice(15)) +
+    '</p><p class="mb-0">' +
+    data.paymentData.expiration +
+    '</p><p class="mb-0">***</p>';
+  $("#billing-details-confirm")
+    .empty()
+    .append(billingDetails);
+  $("#payment-details-confirm")
+    .empty()
+    .append(paymentDetails);
+}
+
 // Cleave input formatting
 
-var cleaveCreditCard = new Cleave("#card-number", {
-  creditCard: true,
-  onCreditCardTypeChanged: cleaveSetCvvAndCardPattern
-});
-
-var cleaveExpiryDate = new Cleave("#expiration", {
-  date: true,
-  datePattern: ["m", "Y"]
-});
-
-var cleaveState = new Cleave("#state-input", {
+var cleaveMailingState = new Cleave("#mailing-state-input", {
   blocks: [2],
   uppercase: true
 });
 
-var cleaveZIP = new Cleave("#zip-input", {
+var cleaveMailingZIP = new Cleave("#mailing-zip-input", {
   numeral: true,
   stripLeadingZeroes: false,
   delimiter: ""
 });
-
-var cleaveExpiryDate = new Cleave("#cvv-code", {
-  numeral: true,
-  stripLeadingZeroes: false,
-  delimiter: ""
-});
-
-function cleaveSetCvvAndCardPattern(type) {
-  if (type === "amex") {
-    $("#card-number").attr("pattern", "[0-9]{4} [0-9]{6} [0-9]{5}");
-    $("#cvv-code").attr({
-      maxlength: 4,
-      pattern: "[0-9]{4}",
-      placeholder: "  1234"
-    });
-  } else {
-    $("#card-number").attr("pattern", "[0-9]{4} [0-9]{4} [0-9]{4} [0-9]{4}");
-    $("#cvv-code").attr({
-      maxlength: 3,
-      pattern: "[0-9]{3}",
-      placeholder: "  123"
-    });
-  }
-  $("#cc-icons-span")
-    .find("i")
-    .removeClass("text-info");
-  var ccSelector = "#cc-icon-" + type;
-  $(ccSelector).addClass("text-info");
-}
 
 // Initialize
 
